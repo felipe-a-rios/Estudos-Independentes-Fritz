1885bc6a47d528cef9bf14b0969fda49c1827358
GabrielBerschSchmidt <gabrielberschschmidt@gmail.com>
2026-06-11 12:31:34 -0300

    Adicionado arquivo relatório para armazenar as funções dos mesmos

---
+++ Relatorios.java
@@ -0,0 +1,460 @@
+public class Relatorios {
+public static void relPed(Arquivo pedidos){
+  int rPed, Poss1, Poss2= 0;
+  String linha = "";
+  int validRelPed = 0;
+  int contPed = 0;
+
+    pedidos.abrirLeitura();
+    linha = pedidos.lerLinha();
+    while (linha != null){
+      contPed++;
+      linha = pedidos.lerLinha();
+    }
+    pedidos.fecharArquivo();
+    linha = "";
+  do {
+    rPed = Entrada.leiaInt(Estudos_independetes.menuJanela("RELATÓRIO PEDIDOS") + "Escolha a opção que você quer acessar\n1- Todos os pedidos \n2- Ped. P/Clientes \n3- Ped. P/Produto \n4- Voltar");
+    switch(rPed){
+      //Todos os pedidos
+      case (1):
+                pedidos.abrirLeitura();
+                linha = pedidos.lerLinha();
+                contPed++;
+                double valorTot = 0;
+
+                String dadosTODOS[] = new String[4];
+                String dadosRel[][] = new String[contPed][4];
+                while (linha != null) {
+                  for(int i = 0; i < contPed - 1; i++){
+                  Poss1 = 0;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosTODOS[0] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosTODOS[1] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosTODOS[2] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  dadosTODOS[3] = linha.substring(Poss1);
+
+                    System.out.println(Estudos_independetes.menu("TODOS OS PEDIDOS"));
+                    System.out.println("CPF: " + dadosTODOS[0]);
+                    System.out.println("Cod. Produto: " + dadosTODOS[1]);
+                    System.out.println("Quantidade: " + dadosTODOS[2]);
+                    System.out.println("Valor Total: " + dadosTODOS[3]);
+                    System.out.println("====================================================== ");
+                    double valConvert = Double.parseDouble(dadosTODOS[3]);
+                    valorTot = valConvert + valorTot;
+                linha = pedidos.lerLinha();
+                  dadosRel[i][0] = dadosTODOS[0];
+                  dadosRel[i][1] = dadosTODOS[1];
+                  dadosRel[i][2] = dadosTODOS[2];
+                  dadosRel[i][3] = dadosTODOS[3];
+                  }
+                }
+                System.out.println("Valor total: " + valorTot);
+                System.out.println("====================================================== ");
+                System.out.println("");
+                System.out.println("");
+                pedidos.fecharArquivo();
+
+                while(validRelPed != 1 && validRelPed != 2){
+                validRelPed = Entrada.leiaInt(Estudos_independetes.menuJanela("SALVAR") + "VOCÊ QUER SALVAR ESSE RELATÓRIO ? \n            1-Sim            2-Não");
+                if(validRelPed == 1){
+                  dadosRel[contPed - 1][0] = Double.toString(valorTot);
+                  saveRel(dadosRel,contPed);
+                  System.out.println(saveRel(dadosRel, contPed));
+                }
+
+                if(validRelPed == 2){
+                  break;
+                }
+
+                if(validRelPed != 1 && validRelPed != 2){
+                  System.out.println(Estudos_independetes.menu("OPÇÃO INVÁLIDA"));
+                }
+              }
+      break;
+      //pedidos por cliente
+      case (2):
+        String nomeBusca = Entrada.leiaString(Estudos_independetes.menuJanela("CONSULTA POR CLIENTE")+"Digite o CPF:");
+
+                pedidos.abrirLeitura();
+                linha = pedidos.lerLinha();
+                double valorTotCli= 0;
+                boolean achouCPF = false;
+                String dadosCPF[] = new String[4];
+
+
+                while (linha != null) {
+                  //String dados[] = linha.split(";");
+                  Poss1 = 0;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCPF[0] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCPF[1] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCPF[2] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  dadosCPF[3] = linha.substring(Poss1);
+
+                  if (dadosCPF[0].toLowerCase().contains(nomeBusca.toLowerCase())) {
+                    System.out.println(Estudos_independetes.menu("PEDIDO POR CLIENTE"));
+                    System.out.println("CPF: " + dadosCPF[0]);
+                    System.out.println("Cod. Produto: " + dadosCPF[1]);
+                    System.out.println("Quantidade: " + dadosCPF[2]);
+                    System.out.println("Valor total: " + dadosCPF[3]);
+                    System.out.println("====================================================== ");
+                    double valConvert = Double.parseDouble(dadosCPF[3]);
+                    valorTotCli = valConvert + valorTotCli;
+                    achouCPF = true;
+                  }
+                  if (!achouCPF){
+                  }
+
+                  linha = pedidos.lerLinha();
+                }
+                System.out.println("Valor total: " + valorTotCli);
+                System.out.println("====================================================== ");
+                System.out.println("");
+                System.out.println("");
+                pedidos.fecharArquivo();
+                if (!achouCPF){
+                  System.out.println(Estudos_independetes.menu("CPF NÃO ENCONTRADO"));
+                  }
+
+      break;
+      case (3):
+        //Pedidos por produto
+        int CodProdRel = Entrada.leiaInt(Estudos_independetes.menuJanela("CONSULTA POR PRODUTO") + "Digite Código do Produto");
+        pedidos.abrirLeitura();
+                linha = pedidos.lerLinha();
+                double valorTotProd = 0;
+                String dadosProd[] = new String[4];
+
+                while (linha != null) {
+                  Poss1 = 0;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosProd[0] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosProd[1] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosProd[2] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  dadosProd[3] = linha.substring(Poss1);  
+
+                  
+                    if (Integer.parseInt(dadosProd[1]) == CodProdRel){
+                    System.out.println(Estudos_independetes.menu("PEDIDOS POR PRODUTO"));
+                    System.out.println("CPF: " + dadosProd[0]);
+                    System.out.println("Cod. Produto: " + dadosProd[1]);
+                    System.out.println("Quantidade: " + dadosProd[2]);
+                    System.out.println("Valor Total: " + dadosProd[3]);
+                    System.out.println("====================================================== ");
+                    double valConvert = Double.parseDouble(dadosProd[3]);
+                    valorTotProd = valConvert + valorTotProd;
+                linha = pedidos.lerLinha();
+                }
+                }
+                System.out.println("Valor total: " + valorTotProd);
+                System.out.println("====================================================== ");
+                System.out.println("");
+                System.out.println("");
+                pedidos.fecharArquivo();
+      break;
+      case (4):
+        System.out.println(Estudos_independetes.menu("VOLTANDO"));
+      break;
+      default:
+        System.out.println(Estudos_independetes.menu("OPÇÃO INVÁLIDA"));
+      break;
+    }
+  } while (rPed != 4);
+  }
+
+public static String saveRel(String[][] dadosRel, int contPed){
+    Arquivo relatorio = new Arquivo("Relatório.txt");
+    String linha = "";
+    String CPF, CodProd,ValorTot,Quant, ValorTotPeds;
+    relatorio.abrirEscrita();
+    linha = ("==========================================================================================================================\n"+
+            "\n"+
+             "                                     \t\t    PEDIDOS\n"+
+            "\n"+
+             "==========================================================================================================================\n");
+    relatorio.escreverLinha(linha);
+    relatorio.fecharArquivo();
+    linha = "";
+    for(int i= 0; i < contPed - 1; i++){
+      CPF = dadosRel[i][0];
+      CodProd = dadosRel[i][1];
+      Quant = dadosRel[i][2];
+      ValorTot = dadosRel[i][3];
+    relatorio.abrirEscrita(true);
+    linha = ("CPF:" + CPF + "\n"+
+             "\n"+
+             "Código do Produto: " + CodProd +"                              Quantidade: " + Quant + " 				Valor total: " + ValorTot + "\n"+
+             "\n"+
+             "==========================================================================================================================\n"+
+             " ");
+    relatorio.escreverLinha(linha);
+    relatorio.fecharArquivo();
+    }
+    ValorTotPeds = dadosRel[contPed - 1][0];
+    linha = ("TOTAL: " + ValorTotPeds);
+    relatorio.abrirEscrita(true);
+    relatorio.escreverLinha(linha);
+    relatorio.fecharArquivo();
+    return Estudos_independetes.menu("RELATÓRIO SALVO COM SUCESSO");
+    }
+      public static void relGrupos(Arquivo produtos){
+    System.out.println(Estudos_independetes.menu("\nRELATÓRIO DE GRUPOS CADASTRADOS"));
+
+    //Vetor para não termos repetições de grupos
+    String[] gruposUnicos = new String[100];
+    int totalDeGrupos = 0;
+    String dadosGrup[] = new String[5];
+    int Poss1, Poss2 = 0;
+    
+    //Abertura do arquivo
+    produtos.abrirLeitura();
+    String linha = produtos.lerLinha();
+
+    while (linha != null){
+                  Poss1 = 0;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosGrup[0] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosGrup[1] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosGrup[2] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  dadosGrup[3] = linha.substring(Poss1);
+      String grupoAtual = dadosGrup[2];
+
+      //Verificação se o grupo ja foi lido
+      boolean jaExiste = false;
+      for (int i = 0; i < totalDeGrupos; i++){
+        if (gruposUnicos[i].equalsIgnoreCase(grupoAtual)){
+          jaExiste = true;
+          break;
+      }
+    }
+    if (!jaExiste){
+      gruposUnicos[totalDeGrupos] = grupoAtual;
+      totalDeGrupos++;
+    }
+    linha = produtos.lerLinha();
+  }
+  produtos.fecharArquivo();
+}
+public static void relsProd(Arquivo produtos){
+        String linha = "";
+        int pr_con = 0;
+            do {
+
+            pr_con = Entrada
+                .leiaInt(Estudos_independetes.menuJanela("CONSULTA PRODUTOS") + "Escolha a forma que voce quer pesquisar\n1 - Codigo\n2 - nome\n3 - Grupo\n4 - Voltar");
+
+            switch (pr_con)  {
+              // Pesquisa por codigo ( pelo numero de produtos que for cadastrando).
+              case (1):
+
+                int codigo = Entrada.leiaInt(Estudos_independetes.menuJanela("CONSULTA POR CÓDIGO")+"Digite o codigo do produto:");
+
+                produtos.abrirLeitura();
+                linha = produtos.lerLinha();
+                int Poss1, Poss2 =0;
+                String dadosCodProd[] = new String[5];
+                boolean encontrou = false;
+
+                while (linha != null) {
+
+                  Poss1 = 0;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCodProd[0] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCodProd[1] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCodProd[2] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  Poss2 = linha.indexOf(';',Poss1);
+                  dadosCodProd[3] = linha.substring(Poss1, Poss2);
+
+                  Poss1 = Poss2 + 1;
+                  dadosCodProd[4] = linha.substring(Poss1);
+
+                  if (Integer.parseInt(dadosCodProd[0]) == codigo) {
+                    System.out.println("Codigo: " + dadosCodProd[0]);
+                    System.out.println("Nome: " + dadosCodProd[1]);
+                    System.out.println("Grupo: " + dadosCodProd[2]);
+                    System.out.println("Estoque: " + dadosCodProd[3]);
+                    System.out.println("Preco: " + dadosCodProd[4]);
+
+                    encontrou = true;
+                    break;
+                  }
+
+                  linha = produtos.lerLinha();
+                }
+
+                produtos.fecharArquivo();
+
+                if (!encontrou) {       
+                  System.out.println(Estudos_independetes.menu("PRODUTO NÂO ENCONTRADO"));
+                }
+                
+
+                break;
+
+
+              // Pesquisa por nome.
+              case (2):
+
+                String nomeBusca = Entrada.leiaString(Estudos_independetes.menuJanela("CONSULTA POR NOME")+"Digite o nome:").toLowerCase();
+
+                produtos.abrirLeitura();
+                linha = produtos.lerLinha();
+                int PossN1, PossN2 =0;
+                String dadosNomProd[] = new String[6];
+                boolean achouNome = false;
+
+                while (linha != null) {
+                  PossN1 = 0;
+                  PossN2 = linha.indexOf(';',PossN1);
+                  dadosNomProd[0] = linha.substring(PossN1, PossN2);
+
+                  Poss1 = PossN2 + 1;
+                  PossN2 = linha.indexOf(';',PossN1);
+                  dadosNomProd[1] = linha.substring(PossN1, PossN2);
+
+                  PossN1 = PossN2 + 1;
+                  PossN2 = linha.indexOf(';',PossN1);
+                  dadosNomProd[2] = linha.substring(PossN1, PossN2);
+
+                  PossN1 = PossN2 + 1;
+                  PossN2 = linha.indexOf(';',PossN1);
+                  dadosNomProd[3] = linha.substring(PossN1, PossN2);
+
+                  PossN1 = PossN2 + 1;
+                  PossN2 = linha.indexOf(';',PossN1);
+                  dadosNomProd[4] = linha.substring(PossN1,PossN2);
+                  
+                  PossN1 = PossN2 + 1;
+                  dadosNomProd[5] = linha.substring(PossN1);
+
+                  if (dadosNomProd[5].contains(nomeBusca)) {
+                    System.out.println("Codigo: " + dadosNomProd[0]);
+                    System.out.println("Nome: " + dadosNomProd[1]);
+                    System.out.println("Grupo: " + dadosNomProd[2]);
+                    System.out.println("Estoque: " + dadosNomProd[3]);
+                    System.out.println("Preco: " + dadosNomProd[4]);
+
+                    achouNome = true;
+                  }
+
+                  linha = produtos.lerLinha();
+                }
+
+                produtos.fecharArquivo();
+
+                if (!achouNome) {
+                  System.out.println(Estudos_independetes.menu("NENHUM PRODUTO ENCONTRADO"));
+                }
+
+                break;
+
+              // Pesquisa pelo grupo(Tres todos os produtos do grupo).
+              case (3):
+                relGrupos(produtos);
+
+                String grupoBusca = Entrada.leiaString(Estudos_independetes.menuJanela("CONSULTA POR GRUPO")+"Digite o grupo:");
+
+                produtos.abrirLeitura();
+                linha = produtos.lerLinha();
+                int PossG1, PossG2 = 0;
+                String dadosGrupProd[] = new String[5];
+                boolean achouGrupo = false;
+
+                while (linha != null) {
+                  PossG1 = 0;
+                  PossG2 = linha.indexOf(';',PossG1);
+                  dadosGrupProd[0] = linha.substring(PossG1, PossG2);
+
+                  PossG1 = PossG2 + 1;
+                  PossG2 = linha.indexOf(';',PossG1);
+                  dadosGrupProd[1] = linha.substring(PossG1, PossG2);
+
+                  PossG1 = PossG2 + 1;
+                  PossG2 = linha.indexOf(';',PossG1);
+                  dadosGrupProd[2] = linha.substring(PossG1, PossG2);
+
+                  PossG1 = PossG2 + 1;
+                  PossG2 = linha.indexOf(';',PossG1);
+                  dadosGrupProd[3] = linha.substring(PossG1, PossG2);
+                  
+                  PossG1 = PossG2 + 1;
+                  dadosGrupProd[4] = linha.substring(PossG1);
+
+                  if (dadosGrupProd[2].equalsIgnoreCase(grupoBusca)) {
+                    System.out.println("Codigo: " + dadosGrupProd[0]);
+                    System.out.println("Nome: " + dadosGrupProd[1]);
+                    System.out.println("Grupo: " + dadosGrupProd[2]);
+                    System.out.println("Estoque: " + dadosGrupProd[3]);
+                    System.out.println("Preco: " + dadosGrupProd[4]);
+
+                    achouGrupo = true;
+                  }
+
+                  linha = produtos.lerLinha();
+                }
+
+                produtos.fecharArquivo();
+
+                if (!achouGrupo) {
+                  System.out.println(Estudos_independetes.menu("NENHUM PRODUTO ENCONTRADO"));
+                }
+
+                break;
+
+              // Finalizar a tela de consulta
+              case (4):
+                System.out.println(Estudos_independetes.menu("VOLTANDO"));
+                break;
+
+              default:
+                System.out.println(Estudos_independetes.menu("OPÇÃO INVÁLIDA"));
+                break;
+            }
+
+          }
+
+            while (pr_con != 4);
+
+}
+}
